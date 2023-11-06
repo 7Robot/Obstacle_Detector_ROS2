4235de722eeeecf408b8c20d01919fca39548e6d
Thomas Bonnefille <thomas.bonnefille@outlook.com>
2023-11-06 22:04:43 +0100

    Add a marker topic
    This feature add a new topic on which the circles detected by the node
    are returned as a spherical marker.
    This facilitates the debugging process on rviz2.
    
    Signed-off-by: Thomas Bonnefille <thomas.bonnefille@outlook.com>

--- include/cluster_detector/cluster_detector.hpp
+++ include/cluster_detector/cluster_detector.hpp
@@ -11,12 +11,15 @@
 #include "cdf_msgs/msg/obstacles.hpp"
 #include "cdf_msgs/msg/circle_obstacle.hpp"
 
+#include "visualization_msgs/msg/marker.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
+
 #include <cmath>
 #include <vector>
 
 #define NB_POINT_SCAN 1600
 #define NB_MEDIAN 2
-#define DIST_CLUSTER 0.2
+#define DIST_CLUSTER 0.1
 #define PI 3.141592653589793
 
 struct PolarPoint {
@@ -34,14 +37,17 @@ private:
     void clusterize();
     void create_message();
     void publish_obstacle();
+    void publish_marker();
     
     int nb_cluster;
+    int nb_marker = 0;
 
     PolarPoint points[NB_POINT_SCAN];
     cdf_msgs::msg::Obstacles buffer_message;
 
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub;
     rclcpp::Publisher<cdf_msgs::msg::Obstacles>::SharedPtr obstacle_pub;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub;
 };
 
 #endif
